1ec7572eff35a2d4537fcd3316709a5afaa7e159
maja74123 <133578971+maja74123@users.noreply.github.com>
2023-06-03 19:33:50 +0200

    Dodano szablon klasy Matrix
    Stworzyłam szablon klasy, która (zgodnie z wymaganiami projektu) przechowuje macierz i jej wymiary.
    Zaimplementowałam niektóre metody, pozostałe są oznaczone przez TODO.
    Trzeba zrobić jeszcze konsolowy interakcyjny interfejs z użytkownikiem, obsługę błędów (rzucanie błędów, łapanie ich tam, gdzie trzeba, i ładny komunikat dla użytkownika).
    Dla wygody do testów dodałam wczytywanie macierzy z pliku, ale trzeba zrobić wczytywanie od użytkownika.

---
+++ Matrix.py
@@ -0,0 +1,138 @@
+def validate_row(row):
+    # TODO zrobić porządną walidację, sprawdzanie isnumeric
+    return list(map(float, row))
+
+
+def read_matrix_from_file(path):
+    # TODO sprawdzanie, czy plik istnieje
+    data = []
+    with open(path, 'r') as file:
+        for line in file:
+            row = validate_row(line.split())
+            data.append(row)
+    number_of_columns = len(data[0])
+    for row in data:
+        if len(row) != number_of_columns:
+            raise ValueError("TODO ładny komunikat")
+    return data
+
+
+def read_matrix_from_user():
+    # TODO wczytywanie i walidacja wejścia od użytkownika
+    pass
+
+
+class Matrix:
+    def __init__(self, data):
+        self.number_of_rows = len(data)
+        self.number_of_columns = len(data[0])
+        self.data = data
+
+    def data_to_string(self):
+        s = ""
+        for row in self.data:
+            s += " ".join(map(str, row))
+            s += "\n"
+        return s
+
+    def __str__(self):
+        return f"Macierz o wymiarach {self.number_of_rows} na {self.number_of_columns} i elementach:\n" + self.data_to_string()
+
+    def print_matrix(self):
+        '''
+        Wypisuje macierz.
+        '''
+        print(self.data_to_string())
+
+    def print_element(self, i, j):
+        '''
+        Wypisuje element macierzy o indeksach i, j.
+        i - indeks wiersza, j - indeks kolumny
+        Uwaga. Indeksowanie od 1 tak jak na algebrze.
+        '''
+        print(self.data[i - 1][j - 1])
+        print()
+
+    def print_row(self, i):
+        '''
+        Wypisuje i-ty wiersz macierzy.
+        '''
+        for element in self.data[i - 1]:
+            print(element, end=' ')
+        print("\n")
+
+    def print_column(self, j):
+        '''
+        Wypisuje j-tą kolumnę macierzy.
+        '''
+        for i in range(0, self.number_of_rows):
+            print(self.data[i][j - 1])
+        print()
+
+    def copy(self):
+        '''
+        Zwraca nowy obiekt typu Matrix, zawierający prawdziwą kopię danych, a nie alias do nich
+        '''
+        data_copy = []
+        for i in range(self.number_of_rows):
+            data_copy.append([])
+            for j in range(self.number_of_columns):
+                data_copy[i].append(self.data[i][j])
+        return Matrix(data_copy)
+
+    def add_elementwise(self, other):
+        '''
+        Zwraca nowy obiekt typu Matrix, będący wynikiem dodania do siebie odpowiadających elementów dwóch macierzy (self i other).
+        '''
+        new_matrix = self.copy()
+        for i in range(self.number_of_rows):
+            for j in range(self.number_of_columns):
+                new_matrix.data[i][j] += other.data[i][j]
+        return new_matrix
+
+    def multiply_by_scalar(self, scalar):
+        '''
+        Zwraca nowy obiekt typu Matrix, będący wynikiem pomnożenia macierzy (czyli self) przez skalar.
+        '''
+        scalar = float(scalar)
+        new_matrix = self.copy()
+        for i in range(self.number_of_rows):
+            for j in range(self.number_of_columns):
+                new_matrix.data[i][j] *= scalar
+        return new_matrix
+
+    def multiply_by_matrix(self, other):
+        # TODO zaimplementować, ma zwracać nowy obiekt typu Matrix, będący iloczynem macierzowym self i other
+        pass
+
+    def determinant(self):
+        # TODO zaimplementować, zwraca wartość wyznacznika dla macierzy kwadratowej (jeśli nie jest kwadratowa, to informuje użytkownika, że wyznacznik nie istnieje)
+        pass
+
+    def inverse(self):
+        # TODO zaimplementować, jeśli wyznacznik jest różny od 0, oblicza i zwraca macierz odwrotną, w przeciwnym razie informuje użytkowanika, że macierz jest osobliwa i odwrotność nie istnieje
+        pass
+
+
+# Poniżej testowanie działania różnych metod.
+
+if __name__ == "__main__":
+    A = Matrix([
+        [1.0, 2.0, 3.0],
+        [4.0, 5.0, 6.0],
+        [7.0, 8.0, 9.0]
+    ])
+
+    A.print_matrix()
+    A.print_row(3)
+    A.print_column(1)
+    A.print_element(2, 2)
+
+    B = Matrix(read_matrix_from_file("Macierz_3x3.txt"))
+    print(B)
+
+    print(A.multiply_by_scalar(6))
+
+    print(A.add_elementwise(A))
+
+    help(Matrix)
